497bbb2459f86493fe2179f273c6b396500d85e7
FYPMVISUALS <fypmvisualsbooking@gmail.com>
2026-07-06 20:33:54 -0500

    Update script.js

--- script.js
+++ script.js
@@ -47,13 +47,13 @@ const games = [
 { week: 3, p1: "Donald", p2: "Daniel", s1: 6, s2: 4 },
 
 /* ================= WEEK 4 ================= */
-{ week: 4, p1: "Mateo", p2: "Carlos", s1: 0, s2: 0 },
+{ week: 4, p1: "Mateo", p2: "Carlos", s1: 5, s2: 6 },
 { week: 4, p1: "Mateo", p2: "Drunoh", s1: 8, s2: 3 },
 { week: 4, p1: "Alec", p2: "Xavier", s1: 3, s2: 4 },
 { week: 4, p1: "Alec", p2: "Donald", s1: 5, s2: 1 },
 { week: 4, p1: "Tyler", p2: "Donald", s1: 6, s2: 1 },
 { week: 4, p1: "Tyler", p2: "Xavier", s1: 6, s2: 8 },
-{ week: 4, p1: "Daniel", p2: "Carlos", s1: 0, s2: 0 },
+{ week: 4, p1: "Daniel", p2: "Carlos", s1: 4, s2: 13 },
 { week: 4, p1: "Daniel", p2: "Drunoh", s1: 0, s2: 0 },
 
 /* ================= WEEK 5 ================= */
